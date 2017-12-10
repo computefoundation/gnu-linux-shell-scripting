fb88f7b8f1cc4a4f646b56da9d9648abefe30049
adrianbartyczak <adrianb@unixfoundation.org>
2017-12-10 14:40:57 -0600

    Update section headers in one-liner files

--- one-liners/one-liners-gnu-linux-main.bash
+++ one-liners/one-liners-gnu-linux-main.bash
@@ -1,14 +1,16 @@
 # one-liners-gnu-linux-main.bash
 # 
 # Sections:
-#    1.   Hardware
+#    1.   Hardware management
 # 
 
-# ======= 1.  HARDWARE =========================================
+# ======= 1.  HARDWARE MANAGEMENT ==============================
 
-# Note: Hardware one-liners are intended to be system-independent, however, some may not work for certain systems.
+# Note: Hardware management one-liners are intended to be distribution-independent, however, some may not work for
+#   certain distributions.
 
 # Set the backlight level.
-# Note: The interface to the ACPI kernel module for video (e.g. intel_backlight) can be found with "ls /sys/class/backlight/".
+# Note: The interface to the ACPI kernel module for video (e.g. intel_backlight) can be found by running
+#   "ls /sys/class/backlight/".
 tee /sys/class/backlight/intel_backlight/brightness <<< <backlight level>
 
--- one-liners/one-liners-gnu-linux-output_only-single-value.bash
+++ one-liners/one-liners-gnu-linux-output_only-single-value.bash
@@ -1,20 +1,22 @@
 # one-liners-gnu-linux-output_only-single-value.bash
 # 
 # Sections:
-#    1.   File
-#    2.   Hardware
+#    1.   File management
+#    2.   Hardware management
 # 
 
-# ======= 1.  FILE =============================================
+# ======= 1.  FILE MANAGEMENT ==================================
 
 # Get the size in megabytes of one or more files and/or directories as a plain number.
 du -csm <files and/or directories> | tail -1 | awk '{ print $1 }'
 
-# ======= 2.  HARDWARE =========================================
+# ======= 2.  HARDWARE MANAGEMENT ==============================
 
-# Note: Hardware one-liners are intended to be system-independent, however, some may not work for certain systems.
+# Note: Hardware management one-liners are intended to be distribution-independent, however, some may not work for
+#   certain distributions.
 
 # Get the maximum backlight level.
-# Note: The interface to the ACPI kernel module for video (e.g. intel_backlight) can be found with "ls /sys/class/backlight/".
+# Note: The interface to the ACPI kernel module for video (e.g. intel_backlight) can be found by running
+#   "ls /sys/class/backlight/".
 cat /sys/class/backlight/intel_backlight/max_brightness
 
