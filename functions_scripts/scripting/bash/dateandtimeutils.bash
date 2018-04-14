09bf65e5bcabdff9248b44008f115da7de817726
adrianbartyczak <adrianb@computingfoundation.org>
2018-04-14 16:24:33 -0500

    Add bash utility functions secondsToMicrosecondsDecimal and secondsToMicroseconds

--- functions_scripts/scripting/bash/dateandtimeutils.bash
+++ functions_scripts/scripting/bash/dateandtimeutils.bash
@@ -9,20 +9,37 @@
 
 # Conversion
 # --------------------------------------------
-  # Convert a time in seconds (using either an integer or decimal) to
-  # millseconds as a decimal.
+  # Convert seconds to milliseconds with decimal precision.
   # Usage:
   #   secondsToMillisecondsDecimal <seconds>
   secondsToMillisecondsDecimal() {
     printf '%.4f' "$(bc <<< "${1}*1000")"
   }
 
-  # Convert a time in seconds (using either an integer or decimal) to
-  # millseconds as an integer rounded to the nearest tenths place (using round
-  # half towards zero mode).
+  # Convert seconds to milliseconds.
   # Usage:
   #   secondsToMilliseconds <seconds>
+  # Notes:
+  #   The number is rounded to the nearest tenths place using half toward zero
+  #   rounding mode. 
   secondsToMilliseconds() {
     printf '%.0f' "$(secondsToMillisecondsDecimal "${1}")"
+  }
+
+  # Convert seconds to microseconds with decimal precision.
+  # Usage:
+  #   secondsToMicrosecondsDecimal <seconds>
+  secondsToMicrosecondsDecimal() {
+    printf '%.4f' "$(bc <<< "${1}*1000000")"
+  }
+
+  # Convert seconds to microseconds.
+  # Usage:
+  #   secondsToMicroseconds <seconds>
+  # Notes:
+  #   The number is rounded to the nearest tenths place using half toward zero
+  #   rounding mode. 
+  secondsToMicroseconds() {
+    printf '%.0f' "$(secondsToMicrosecondsDecimal "${1}")"
   }
 
